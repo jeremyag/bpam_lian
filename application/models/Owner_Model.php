4f670ffdc6dd5dc02e134a225e04c59a213fa270
Jeremy Agcaoili <ojt.jeremy.agcaoili@luxis.co>
2019-02-24 18:09:10 +0800

    Created Owner Model.

---
+++ application/models/Owner_Model.php
@@ -0,0 +1,35 @@
+<?php
+    class Owner{
+        public $id;
+        public $tin;
+        public $last_name;
+        public $first_name;
+        public $middle_name;
+        public $street;
+        public $city;
+        public $province;
+        public $postal_code;
+        public $email;
+        public $mobile;
+        public $telephone;
+
+        public function set_instance_array($arr){
+            $this->id = $arr['id'];
+            $this->tin = $arr['tin'];
+            $this->last_name = $arr['last_name'];
+            $this->first_name = $arr['first_name'];
+            $this->middle_name = $arr['middle_name'];
+            $this->street = $arr['street'];
+            $this->city = $arr['city'];
+            $this->province = $arr['province'];
+            $this->postal_code = $arr['postal_code'];
+            $this->email = $arr['email'];
+            $this->mobile = $arr['mobile'];
+            $this->telephone = $arr['telephone'];
+        }
+    }
+
+    class Owner_Model extends CI_Model{
+
+    }
+?>
