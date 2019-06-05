2e5e85d5b84c8e5717c12b2ceb47aafaec38bd07
Jeremy Agcaoili <agcaoili.jeremy@gmail.com>
2019-06-05 10:30:21 +0800

    Added front-end for settings

--- application/views/admin/application/view_application_view.php
+++ application/views/admin/application/view_application_view.php
@@ -143,4 +143,4 @@
     let business = <?=$business->id?>;
     let application = <?=$application->id?>;
 </script>
-<script src="<?php echo base_url().add_index();?>assets/js/applications/view_applications.js"></script>
+<script src="<?php echo base_url();?>assets/js/applications/view_applications.js"></script>
