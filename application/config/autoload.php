01af764713b5daf75aeeb2a87dee380bbdb89d2c
Jeremy Agcaoili <agcaoili.jeremy@gmail.com>
2019-06-08 16:57:28 +0800

    Autoload debug and pagination helpers

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'form', 'path', 'hidders');
+$autoload['helper'] = array('url', 'form', 'path', 'hidders', 'debug', 'pagination');
 
 /*
 | -------------------------------------------------------------------
